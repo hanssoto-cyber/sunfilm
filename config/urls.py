a592cb4e60e86ae3687c1a66dce591249b3156ce
hanssoto-cyber <hans.soto.g@gmail.com>
2026-08-15 17:44:59 -0400

    Restaurar URL admin configurable via ADMIN_URL (versionado en Git)

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(os.environ.get('ADMIN_URL', 'admin/'), admin.site.urls),
     path('servicios/', include('servicios.urls')),
     path('galeria/', include('galeria.urls')),
     path('tips/', include('blog.urls')),
