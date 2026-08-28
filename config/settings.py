1dd7988d0c3593a1d1e11e63e6ef344ac34f147c
hanssoto-cyber <hans.soto.g@gmail.com>
2026-08-28 17:15:45 -0400

    security: agregar cabecera Permissions-Policy

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'axes.middleware.AxesMiddleware',
+    'core.middleware.PermissionsPolicyMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
